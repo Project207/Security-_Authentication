7b3b86a03f61e9a21fa0f48fcef2f95cba82989d
rahul <rajpurohitrahul186@gmail.com>
2022-07-26 17:25:38 +0530

    Bcrypt Added and Md5 Removed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //jshint esversion:6
 const express = require("express")
 const app = express()
-const md5 =require("md5")
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended:true}))
@@ -52,34 +53,47 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
    
-    const newUser = new User({
-       email:req.body.username,
-       password:md5(req.body.password)
-    })
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+       
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err)
-        }else{
-            res.render("secrets");
-        }
-    }); 
+        const newUser = new User({
+            email:req.body.username,
+            password:hash
+         })
+     
+         newUser.save(function(err){
+             if(err){
+                 console.log(err)
+             }else{
+                 res.render("secrets");
+             }
+         }); 
+
+
+    });
+   
+    
  });
 
  app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = md5(req.body.password);
 
-    User.findOne({email:username},function(err,foundUser){
-        if(foundUser){
-            if(foundUser.password === password){
-                res.render("secrets");}
-            else{
-                res.send("Invalid Password")
+    const username = req.body.username;
+        const password = req.body.password;
+    
+        User.findOne({email:username},function(err,foundUser){
+
+            if(foundUser){
+                bcrypt.compare(password,foundUser.password,function(err,result){
+
+                    if(result===true){
+                        res.render("secrets");
+                    } 
+                    else {res.send("Invalid password") }
+                 })
             }
-        }
-        else{ 
-            res.send("Invalid Email")
-        }
-    })
- })
+            else{ 
+                res.send("Invalid Email")
+            }
+        });
+
+ });
